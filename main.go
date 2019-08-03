dffa82085a1082b6e84d9209db97f0b06b78fbb8
akshay.khanna <akshay.khanna@thoughtworks.com>
2019-08-03 17:47:59 +0530

    Akshay | Add config & generate swagger

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/files"
 	"strconv"
+	"./docs"
 )
 
 var router *gin.Engine
@@ -13,25 +16,44 @@ type Users struct {
 	Lastname  string `gorm:"not null" form:"lastname" json:"lastname"`
 }
 
+// @contact.name API Support
+// @contact.url http://www.swagger.io/support
+// @contact.email support@swagger.io
+
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+
+// @termsOfService http://swagger.io/terms/
 func main() {
+
+	// programatically set swagger info
+	docs.SwaggerInfo.Title = "Swagger Example API"
+	docs.SwaggerInfo.Description = "This is a sample server Petstore server."
+	docs.SwaggerInfo.Version = "1.0"
+	docs.SwaggerInfo.Host = "petstore.swagger.io"
+	docs.SwaggerInfo.BasePath = "/v2"
+	docs.SwaggerInfo.Schemes = []string{"http", "https"}
 	r := gin.Default()
 
 	v1 := r.Group("api/v1")
 	{
-		v1.POST("/users", PostUser)
 		v1.GET("/users", GetUsers)
 		v1.GET("/users/:id", GetUser)
-		v1.PUT("/users/:id", UpdateUser)
-		v1.DELETE("/users/:id", DeleteUser)
 	}
-
+	// use ginSwagger middleware to serve the API docs
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	r.Run(":8080")
 }
 
-func PostUser(c *gin.Context) {
-	// The futur code…
-}
 
+// GetUsers godoc
+// @Summary List users
+// @Description get users
+// @Accept  json
+// @Produce  json
+// @Success 200 {array} users
+// @Header 200 {string} Token "qwerty"
+// @Router /users [get]
 func GetUsers(c *gin.Context) {
 	var users = []Users{
 		Users{Id: 1, Firstname: "Oliver", Lastname: "Queen"},
@@ -43,6 +65,16 @@ func GetUsers(c *gin.Context) {
 	// curl -i http://localhost:8080/api/v1/users
 }
 
+// GetUser godoc
+// @Summary Get user by
+// @Description get int by ID
+// @ID get-string-by-int
+// @Produce  json
+// @Param id path int true "ID"
+// @Success 200 {object} Users
+// @Header 200 {string} Token "qwerty"
+// @Failure 404
+// @Router users/{id} [get]
 func GetUser(c *gin.Context) {
 	id := c.Params.ByName("id")
 	user_id, _ := strconv.ParseInt(id, 0, 64)
@@ -59,12 +91,4 @@ func GetUser(c *gin.Context) {
 	}
 
 	// curl -i http://localhost:8080/api/v1/users/1
-}
-
-func UpdateUser(c *gin.Context) {
-	// The futur code…
-}
-
-func DeleteUser(c *gin.Context) {
-	// The futur code…
 }
